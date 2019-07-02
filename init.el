42b51406dee20e99fd91da56eb199f42e86537ff
atticode <atticode@gmail.com>
2019-07-02 16:12:25 +0800

    init

--- init.el
+++ init.el
@@ -51,19 +51,3 @@
 ;; docker
 (require 'init-docker)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (web-mode vue-mode transient toml-mode tide slime scss-mode sass-mode robe markdown-mode magit lua-mode lsp-mode julia-mode json-mode js2-mode htmlize haskell-mode fill-column-indicator erlang emmet-mode elixir-mode dockerfile-mode docker-compose-mode dart-mode csharp-mode company-web company-php company-go auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
