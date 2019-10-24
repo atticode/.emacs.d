4ba51b800c90baa97c35b5dc41b19427ad53db8d
atticode <atticode@gmail.com>
2019-10-24 17:39:13 +0800

    php

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -1,7 +1,30 @@
 ;; PHP
+;; https://github.com/emacs-php/php-mode
+;; https://github.com/xcwen/ac-php
 
 (install-package 'php-mode)
-(install-package 'company-php)
+(install-package 'ac-php)
+;;(install-package 'company-php)
+
+(require 'php-mode)
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+
+             ;; Enable ElDoc support (optional)
+             (ac-php-core-eldoc-setup)
+
+             (set (make-local-variable 'company-backends)
+                  '((company-ac-php-backend company-dabbrev-code)
+                    company-capf company-files))
+
+             ;; Jump to definition (optional)
+             (define-key php-mode-map (kbd "M-]")
+               'ac-php-find-symbol-at-point)
+
+             ;; Return back (optional)
+             (define-key php-mode-map (kbd "M-[")
+               'ac-php-location-stack-back)))
 
 
 ;;(setq php-manual-path "/usr/share/doc/php-manual/en/html/")
